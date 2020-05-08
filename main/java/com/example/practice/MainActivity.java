a5fe8470d318168b05dd3d6c73b417a9e59e237d
adarsh-nl <47457585+adarsh-nl@users.noreply.github.com>
2020-05-08 14:22:45 +0530

    Add files via upload

---
+++ main/java/com/example/practice/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    public void fade(View view){
+        ImageView ironman1= (ImageView) findViewById(R.id.im1);
+        ironman1.animate().translationXBy(-2000f).setDuration(2000);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
